85e02816869ff998f5ac8c576fec540d67faa5d8
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2025-01-03 14:34:57 +0100

    first push, section navigation working

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,31 @@
+import NavBar from "./NavBar";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  // const [isScrolled, setIsScrolled] = useState(false);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const element = document.getElementById("movieList");
+  //     if (element) {
+  //       const offset = element.offsetTop - 200;
+
+  //       const scrollTop = window.scrollY;
+  //       setIsScrolled(scrollTop > offset);
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  return (
+    <header>
+      <NavBar />
+    </header>
+  );
+}
---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  const handleButton = (elementInput: string) => {
+    const element = document.getElementById(elementInput);
+
+    if (element) {
+      // Calculate the offset by subtracting 100 pixels from the top of the element
+      const offset = element.offsetTop - 20;
+
+      // Scroll to the calculated offset position
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button onClick={() => handleButton("firstSection")}>
+            First Section
+          </button>
+        </li>
+        <li>
+          <button onClick={() => handleButton("secondSection")}>
+            Second Section
+          </button>
+        </li>
+        <li>
+          <button onClick={() => handleButton("thirdSection")}>
+            Third Section
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
