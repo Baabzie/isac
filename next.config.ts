a64b06e07a57b19a9fc1bf64f7bc7a08ade7627a
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2025-01-29 22:11:30 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "" : "",
+  basePath: isProd ? " " : "",
   reactStrictMode: true,
   output: "export",
 };
